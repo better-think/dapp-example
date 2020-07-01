0bf854bf495911dc56c3cac4e0944f43c4e42997
chaojin752 <chaojin752@gmail.com>
2020-07-01 10:58:29 +0200

    styled store

--- app/src/components/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './Logo.css';
-
-const logo = () => (
-    <span className={classes.ProjectLogo}>Albom</span>
-);
-
-export default logo;
--- app/src/ipfs.js
+++
@@ -1,5 +0,0 @@
-const ipfsClient = require('ipfs-http-client');
-
-const ipfs = ipfsClient('https://ipfs.infura.io:5001/');
-
-export default ipfs;
