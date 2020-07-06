7e09845e97dca67bf823d54b568e1c149263a4a2
chaojin752 <chaojin752@gmail.com>
2020-07-06 15:12:34 +0200

    first deployment

--- app/src/containers/NiftyMinter/NiftyMinter.js
+++ app/src/containers/NiftyMinter/NiftyMinter.js
@@ -135,6 +135,8 @@ class NiftyMinter extends React.Component {
             }
         });
 
+        console.log(this.props.drizzleState.accounts[0])
+
         this.props.drizzle.contracts.Collectible.methods.multiMint
             .cacheSend(
                 nifties,
--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -3,10 +3,24 @@ import Web3 from "web3";
 import Collectible from "./contracts/Collectible.json";
 
 
+let web3 = null;
+
+if (window.ethereum) {
+    web3 = new Web3(window.ethereum)
+    window.ethereum.enable().catch(error => {
+        // User denied account access
+        console.log(error)
+    });
+}
+else {
+    web3 = new Web3("wss://ropsten.infura.io/ws/v3/27b9545d910143088317fba41303fbe6");
+}
+
+
 const options = {
     web3: {
         block: false,
-        customProvider: new Web3("ws://localhost:8545"),
+        customProvider: web3,
     },
     contracts: [Collectible],
     events: {},
